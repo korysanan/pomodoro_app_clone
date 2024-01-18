beed55ac22d8fc5350c5faa5e6758a7fb8c7c024
korysanan <ujin0561@naver.com>
2024-01-19 01:27:49 +0900

    빨초파 분할

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Flexible(
+            flex: 1,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.red,
+              ),
+            ),
+          ),
+          Flexible(
+            flex: 2,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.green,
+              ),
+            ),
+          ),
+          Flexible(
+            flex: 1,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
